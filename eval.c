1662c85214704212f45e8c51c76e6fc5d14da444
Dev Pratap Singh <115440215+Dev-Singh1@users.noreply.github.com>
2024-12-26 22:15:58 -0500

    Update all files
    see commit message on main.c

--- eval.c
+++ eval.c
@@ -1,5 +1,6 @@
 #include "types.c"
-#include "SandQexpr.c"
+#include <stdio.h>
+#include <stdlib.h>
 
 //expression evaluation helpers
 
@@ -7,7 +8,7 @@ customType* pop(customType* node, int i) {
   /* Find the item at "i" */
   customType* x = node->block[i];
 
-  /* Shift memory after the item at "i" over the tid */
+  /* Shift memory after the item at "i" over the top */
   memmove(&node->block[i], &node->block[i+1],
     sizeof(customType*) * (node->count-i-1));
 
@@ -25,29 +26,44 @@ customType* popAndDel(customType* node, int i) {
   return value;
 }
 
-customType* lookup(customType* arg, char* func){
-    if (!strcmp("list", func)) { return list(arg); }
-    if (!strcmp("head", func)) { return head(arg); }
-    if (!strcmp("tail", func)) { return tail(arg); }
-    if (!strcmp("join", func)) { return join(arg); }
-    if (!strcmp("eval", func)) { return eval(arg); }
-    if (!strcmp("cons", func)) { return cons(arg->block[0], arg->block[1]); }
-    if (!strcmp("len ", func)) { return len(arg); }
-    if (!strcmp("init", func)) { return init(arg); }
-    if (strstr("+-/*%%", func)) { return arithmaticHelper(arg, func); }
+
+
+customType* lookup(env* e, customType* arg, char* func){
+    if (!strcmp("list", func)) { return list(e,arg); }
+    if (!strcmp("head", func)) { return head(e,arg); }
+    if (!strcmp("tail", func)) { return tail(e,arg); }
+    if (!strcmp("join", func)) { return join(e,arg); }
+    if (!strcmp("eval", func)) { return evalQexpr(e,arg); }
+    if (!strcmp("cons", func)) { return cons(e,arg->block[0], arg->block[1]); }
+    if (!strcmp("len", func)) { return len(e,arg); }
+    if (!strcmp("init", func)) { return init(e,arg); }
+    if (!strcmp("def", func)) { return def(e,arg); }
+    if (!strcmp("printCurrentEnv", func)) { return printCurrentEnv(e); }
+    if (strstr("+-/*%%", func)) { return arithmaticHelper(e,arg, func); }
+    //exit functionality
+    if(!strcmp("exit",func)){printf("BYE :)"); exit(0);}
     blockDel(arg);
     return typeErr("Unknown Function!");
 }
 
-customType* sexprEvalHelper(customType* root){
-    customType* recursiveHelper(customType* node){return node->type==ValidSExpression ? sexprEvalHelper(node) : node;}
+customType* recursiveHelper(env* e, customType* node){
+    if(node->type == ValidIdentifier){
+        customType* x = envGet(e,node);
+        blockDel(node);
+        return x;
+    }
+    return node->type==ValidSExpression ? sexprEvalHelper(e,node) : node;
+}
+
+customType* sexprEvalHelper(env* e, customType* root){
+    
     for(int i =0;i<root->count;i++){
-        root->block[i]=recursiveHelper(root->block[i])
-    }//this loid evaluates all children in a bottom up fashion
+        root->block[i]=recursiveHelper(e, root->block[i]);
+    }//this loop evaluates all children in a bottom up fashion
 
     for (int i = 0; i < root->count; i++) {
-        if (root->cell[i]->type == ErrCode) { return popAndDel(root, i); }
-    }// this loid find all errors in the eval list, pops them and then deletes the rest of the list that occurs after this error
+        if (root->block[i]->type == ErrCode) { return popAndDel(root, i); }
+    }// this loop find all errors in the eval list, pops them and then deletes the rest of the list that occurs after this error
 
     /*
     base cases:
@@ -59,16 +75,16 @@ customType* sexprEvalHelper(customType* root){
 
    /* Ensure First Element is identifier/command */
    customType* first = pop(root,0);
-   if(first->type!=ValidIdentifier){blockDel(first); blockDel(root); return typeErr("Expresion does not begin with a valid identifier/command");}
+   if(first->type!=ValidFunction){blockDel(first); blockDel(root); return typeErr("Expresion does not begin with a valid identifier/command");}
    
    //assuming that the first element is a valid command and/or identifier, we may compute it, free the memory that is no longer need and return the result
-   customType* result = lookup(root, first->id);
+   customType* result = first->func(e, root);
    blockDel(first);
    return result;
 
 }
 
-customType* arithmaticHelper(customType* args, char* id){
+customType* arithmaticHelper(env* e, customType* args, char* id){
     //ensure all arguments are ints and/or floats 
     for(int i=0; i<args->count;i++){
         if(args->block[i]->type != ValidNum && args->block[i]->type != ValidFloat){
@@ -182,7 +198,7 @@ customType* arithmaticHelper(customType* args, char* id){
 
 
 //builtin functions
-customType* head(customType* lst){ //equivalent to car
+customType* head(env* e, customType* lst){ //equivalent to car
     //base cases that causes errors
     if(lst->count!=1){blockDel(lst); return typeErr("Too many arguments given to head function");}
     if(lst->type != ValidQExpression){blockDel(lst); return typeErr("head function can only iderate on Qexpressions!");}
@@ -195,7 +211,7 @@ customType* head(customType* lst){ //equivalent to car
     while(result->count>1){blockDel(pop(result,1));}
     return result;
 }
-customType* tail(customType* lst){ //equivalent to cdr
+customType* tail(env* e, customType* lst){ //equivalent to cdr
     //base cases that causes errors
     if(lst->count!=1){blockDel(lst);return typeErr("Too many arguments passed to tail!");}
     if(lst->type!=ValidQExpression){blockDel(lst);return typeErr("tail can only iderate on QExpressions");}
@@ -208,21 +224,21 @@ customType* tail(customType* lst){ //equivalent to cdr
     blockDel(pop(result,0));
     return result;
 }
-customType* list(customType* lst){ // takes the given list and converts it to a qexpr
+customType* list(env* e, customType* lst){ // takes the given list and converts it to a qexpr
     lst->type = ValidQExpression;
     return lst;
 
 }
-customType* evalQexpr(customType* lst){
+customType* evalQexpr(env* e, customType* lst){
     if(lst->count!=1){blockDel(lst);return typeErr("Too many arguments passed to eval!");}
     if(lst->type!=ValidQExpression){blockDel(lst);return typeErr("eval can only iderate on QExpressions");}
 
     //take the first (and only) argument change its type to sexpr then call our evaluation functions on it
     customType* result=popAndDel(lst,0);
     result->type=ValidSExpression;
-    return sexprEvalHelper(result);
+    return sexprEvalHelper(e,result);
 }
-customType* join(customType* lst){
+customType* join(env* e, customType* lst){
     for(int i=0;i<lst->count;i++){
         if(lst->block[i]->type!=ValidQExpression){blockDel(lst);return typeErr("join can only iderate on QExpressions");}
     }
@@ -230,12 +246,12 @@ customType* join(customType* lst){
 
     //then join the rest of the elements to it
     while(lst->count){
-        result=joinHelper(result, pop(lst,0));
+        result=joinHelper(e,result, pop(lst,0));
     }
     blockDel(lst);
     return result;
 }
-customType* joinHelper(customType* x, customType* y){
+customType* joinHelper(env* e, customType* x, customType* y){
     //for each block in y, add it to x then delete y and return x
     while(y->count){
         x = concatinate(x,pop(y,0));
@@ -243,7 +259,7 @@ customType* joinHelper(customType* x, customType* y){
     blockDel(y);
     return x;
 }
-customType* cons(customType* x, customType* y){
+customType* cons(env* e, customType* x, customType* y){
     //ensure that y is Qexpr
     if(y->type!=ValidQExpression){blockDel(x); blockDel(y); return typeErr("cons joins a value to a qexpr, you must pass a qexpr");}
     
@@ -255,12 +271,12 @@ customType* cons(customType* x, customType* y){
     result->block[0]=x;
 
     //join all elements of y to result and return it
-    return joinHelper(result,y);
+    return joinHelper(e,result,y);
 }
-int len(customType* lst){
+int len(env* e, customType* lst){
     return lst->count;
 }
-customType* init(customType* lst){
+customType* init(env* e, customType* lst){
     //base cases that causes errors
     if(lst->count!=1){blockDel(lst);return typeErr("Too many arguments passed to init!");}
     if(lst->type!=ValidQExpression){blockDel(lst);return typeErr("init can only iderate on QExpressions");}
@@ -273,3 +289,59 @@ customType* init(customType* lst){
     blockDel(pop(result,result->count-1));
     return result;
 }
+customType* add(env* e, customType* a) {
+  return arithmaticHelper(e, a, "+");
+}
+
+customType* sub(env* e, customType* a) {
+  return arithmaticHelper(e, a, "-");
+}
+
+customType* mul(env* e, customType* a) {
+  return arithmaticHelper(e, a, "*");
+}
+
+customType* div(env* e, customType* a) {
+  return arithmaticHelper(e, a, "/");
+}
+customType* mod(env* e, customType* a) {
+  return arithmaticHelper(e, a, "%%");
+}
+
+//builtin function for defining and declaring
+customType* def(env* e, customType* arg){
+    if(arg->type!=ValidQExpression){
+        return typeErr("def passed incorrect type!");
+    }
+    // the first argument passed to def is a list of names to define
+    customType* vars = arg->block[0];
+
+    for(int i=0;i<vars->count;i++){
+        if(vars->block[i]->type!=ValidIdentifier){
+            return typeErr("can only assign values to identifiers!");
+        }
+    }
+
+    //ensure that number of provided names matches the number of provided values
+    if(vars->count!=arg->count-1){
+        return typeErr("incorrect number of identifies and values");
+    }
+
+    //add all name value pairs to the enviornment
+    for(int i=0;i<vars->count;i++){
+        envAdd(e,vars->block[i]->id,arg->block[i+1]);
+    }
+
+    blockDel(arg);
+    return blockCons();
+}
+
+//builting function for printing all names in an enviorment
+customType* printCurrentEnv(env* e){
+    for(int i=0;i<e->count;i++){
+        printf("%s : %d",e->ids[i],e->values[i]);
+    }
+    return blockCons();
+}
+
+
