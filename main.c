62966aa4267f9d40a6d19930851ad8db307a4330
Dev Pratap Singh <115440215+Dev-Singh1@users.noreply.github.com>
2024-12-26 22:14:33 -0500

    Update all files
    added support for user defined variables

--- main.c
+++ main.c
@@ -2,13 +2,55 @@
 #include <stdlib.h>
 #include "mpc.h"
 #include "types.c"
-#include "SandQexpr.c"
 #include "readAST.c"
 #include "printResult.c"
 #include "eval.c"
 
-static char input[2048]
+//apparently, according to official documentation, readline and addhistory are function that dont exist on windows 32 bit so i have to define an edgecase in which I make my own if I am compling on/for a 32 bit windows system
+#ifdef _WIN32
 
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+
+//register builtins into the enviorment
+void builtinFunctionAddHelper(env* e, char* name, funcPtr f){
+    customType* x = typeIdentifier(name);
+    customType* y = typeFunction(f);
+    envAdd(e,x,y);
+    blockDel(x);blockDel(y); 
+}
+void builtinFunctionAdd(env* e){
+    builtinFunctionAddHelper(e, "list", list);
+    builtinFunctionAddHelper(e, "head", head);
+    builtinFunctionAddHelper(e, "tail", tail);
+    builtinFunctionAddHelper(e, "eval", evalQexpr);
+    builtinFunctionAddHelper(e, "join", join);
+    builtinFunctionAddHelper(e, "cons", cons);
+    builtinFunctionAddHelper(e, "init", init);
+    builtinFunctionAddHelper(e, "len", len);
+    builtinFunctionAddHelper(e, "def", def);
+    builtinFunctionAddHelper(e, "/", div);
+    builtinFunctionAddHelper(e, "*", mul);
+    builtinFunctionAddHelper(e, "+", add);
+    builtinFunctionAddHelper(e, "-", sub);
+    builtinFunctionAddHelper(e, "%%", mod);
+}
 
 int main(int argc, char** argv){
     /* Create Some Parsers */
@@ -27,31 +69,35 @@ int main(int argc, char** argv){
         "                                                     \
             number   : /-?[0-9]+/;                           \
             float    : /-?[0-9]+.?[0-9]+/ \
-            identifier : '+' | '-' | '*' | '/' | '%%' | \"list\" | \"head\" | \"tail\"                \
-           | \"join\" | \"eval\" | \"cons\" | \"len\" | \"init\";                \
+            identifier : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;                \
             sexpr   : '(' <expr>* ')' \
             qexpr   : '{' <expr>* '}' \
             expr     : <number> | <float> | '<sexpr>' ;\
             program    : /^/ <identifier> <expr>+ /$/ ;         \
         ",
         Number, Float, Identifier, Sexpr, Qexpr, Expr, Program);
+    
+    env* e = newEnv();
+    builtinFunctionAdd(e);
 
     while(true){
-        printf(">");
+        char* input = readline(">");
+        add_history(input);
 
-        fget(input, 2048, stdin);
-        
         mpc_result_t ast;
         if(mpc_parse("<stdin>",input,Program,&ast)){
             // if successfull, print the result of evaluating the ast
-            customType output = recursiveEvalHelper(ast.output);
+            customType* output = recursiveHelper(e,readAll(ast.output));
             extendedPrintf(output);
+            blockDel(output)
             mpc_ast_delete(ast.output);
         }else{
             mpc_ast_print(ast.error);
             mpc_ast_delete(ast.error);
         }
+        free(input);
     }
+    delEnv(e);
     /* Undefine and Delete our Parsers */
     mpc_cleanup(7,Number,FLoat,Identifier,Sexpr,Qexpr,Expr,Program);
     return 0
